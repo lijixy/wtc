7f1fcbe4cd1fd872400beb9e468a66383fcfe55c
Mark <Mark@Mark-PC>
2019-07-08 09:37:56 +0800

    修改

--- wswtc.js
+++ wswtc.js
@@ -16,7 +16,7 @@
  * Configurables.
  */
 const TESTING = false
-const RPC_SERVICE = TESTING ? 'http://39.108.147.77:47402' : 'http://39.108.147.77:47402'
+const RPC_SERVICE = TESTING ? 'http://127.0.0.1:8545' : 'http://127.0.0.1:8545'
 const CHAIN_ID = TESTING ? 15 : 15
 const SERVER_PORT = 9520 // Connector service port
 const BIND_IP = TESTING ? '0.0.0.0' : '0.0.0.0'
