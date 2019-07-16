4a6a3aa9aaec219cc50d6fe5993881a0bb519052
Mark <Mark@Mark-PC>
2019-07-16 15:51:51 +0800

    修改

--- wshpb.js
+++ wshpb.js
@@ -149,7 +149,7 @@ async function loadTxData(plist) {
     }
     if (!checkAddr(plist[2])) return null
     txdata.from = plist[2]
-    txdata.nonce = await web3.hpb_getTransactionCount(plist[2])
+    txdata.nonce = await web3.hpb.hpb_getTransactionCount(plist[2])
     if (!checkAddr(plist[3])) return null
     txdata.to = plist[3]
     txdata.gasPrice = plist[4]
